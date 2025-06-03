f64938731716e3d5b732526bb01a1ec9e86ecfad
Manjushree8 <manjushreekpm08@gmail.com>
2025-06-03 11:40:41 +0530

    Merged remote with local

---
+++ products.js
@@ -0,0 +1,83 @@
+const products = [
+    {
+        id:1,
+        name:"Croped Cotton Pant",
+        src:"images/products/p1.jpg",
+        desc:"",
+        price:220,
+        tags:["new","white","summer"]
+    },
+
+    {
+        id:2,
+        name:"Cotton Set",
+        src:"images/products/p2.jpg",
+        desc:"",
+        price:260,
+        tags:["new","beach"]
+    },
+
+    {
+        id:3,
+        name:"White TShirt",
+        src:"images/products/p3.jpg",
+        desc:"",
+        price:399,
+        tags:["old","white","party"]
+    },
+
+    {
+        id:4,
+        name:"Printed Summer Shirt",
+        src:"images/products/p4.jpg",
+        desc:"",
+        price:399,
+        tags:["old","white","beach"]
+    },
+    {
+        id:5,
+        name:"Yellow Shirt",
+        src:"images/products/p5.jpg",
+        desc:"",
+        price:579,
+        tags:["old","beach"]
+    }
+,
+    {
+        id:6,
+        name:"IndoWestern Pnat",
+        src:"images/products/p6.jpg",
+        desc:"",
+        price:579,
+        tags:["old","green","white","party"]
+    },
+
+    {
+        id:7,
+        name:"Party Floral Jean",
+        src:"images/products/p7.jpg",
+        desc:"",
+        price:399,
+        tags:["old","blue","party"]
+    },
+    {
+        id:8,
+        name:"Beach Shirt",
+        src:"images/products/p8.jpg",
+        desc:"",
+        price:579,
+        tags:["old","green","white","beach"]
+    },
+    {
+        id:9,
+        name:"Shirt Party Red",
+        src:"images/products/p9.jpg",
+        desc:"",
+        price:220,
+        tags:["new","red","summer"]
+    },
+
+
+]
+
+export {products}
